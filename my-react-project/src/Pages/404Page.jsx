3d343687a29bc62c32d5299ac2495a87223b9e04
Gerwin Mateo <Gerwin.mateo@yahoo.com>
2024-07-23 18:27:47 -0700

    Added new thing
    Tried to see if commits work

--- my-react-project/src/Pages/404Page.jsx
+++ my-react-project/src/Pages/404Page.jsx
@@ -6,6 +6,12 @@ function ErrorPage() {
         <>
         <Header />
         <p>404 ERROR: The requested URL was not found</p>
+
+        <p>Gerwin here – I’m SORRY! 
+        Let me entertain you with something fun while you’re here.
+        Try to click the button that appears!
+        </p>
+
         <Footer />
         </>
     )
